e0f8e33f9a2b29c0b611a3c9d72c1ffff0cdb29b
raviw45 <ravikantwaghmare82@gmail.com>
2025-03-23 13:17:34 +0530

    fix: ESlint corrections for deployment

--- components/user/AllProducts.tsx
+++ components/user/AllProducts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useDeleteProduct, useGetProducts } from "@/features/useProduct";
 import { useGetVouchers } from "@/features/useOrder";
